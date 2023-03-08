74e36692c8eb91db477660a1b3d4bd74add49e14
Anvesh Karra <anveshkarra1234@gmail.com>
2023-03-08 15:24:18 +0530

    Added token gen logic from sign in

--- src/controllers/user_controller.js
+++ src/controllers/user_controller.js
@@ -26,6 +26,27 @@ const create = async (req, res) => {
     }
 }
 
+const signIn = async (req, res) => {
+    try {
+        const response = await userService.signIn(req.body.email , req.body.password);
+        return res.status(200).json({
+            message: 'Successfully signed in',
+            success: true,
+            data: response,
+            error: {}
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message: 'Something went wrong',
+            success: false,
+            data: {},
+            error: error
+        });
+    }
+}
+
 module.exports = {
-    create
+    create ,
+    signIn
 }
