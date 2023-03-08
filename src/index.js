d34686dd7629d549a46d853078dc38a61cf4fb09
Anvesh Karra <anveshkarra1234@gmail.com>
2023-03-08 11:56:20 +0530

    Added jwt create and verify logic

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const {PORT} = require('./config/serverConfig');
 
 const apiRoutes = require('./routes/index');
 
+const UserService = require('./services/user_service');
+
 const app = express();
 
 const prepareAndStartServer = async () => {
@@ -15,6 +17,14 @@ const prepareAndStartServer = async () => {
     
     app.listen(PORT , () => {
         console.log(`Server Started on Port: ${PORT}`);
+
+        // const service = new UserService();
+        // const newToken = service.createToken({email: 'anvesh@admin.com' , id: 1});
+        // console.log('new token is',newToken);
+
+        // const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFudmVzaEBhZG1pbi5jb20iLCJpZCI6MSwiaWF0IjoxNjc4MjU2NTE0LCJleHAiOjE2NzgyNTY1NDR9.5pYoyxWVjrhKW4_92SwI4uJtyQ3EhgLERKIk-k-es1Q";
+        // const response = service.verifyToken(token);
+        // console.log(response);
     });
 }
 
--- src/services/user_service.js
+++ src/services/user_service.js
@@ -1,4 +1,6 @@
 const UserRepository = require('../repository/user_repository');
+const { JWT_KEY } = require('../config/serverConfig');
+const jwt = require('jsonwebtoken');
 
 class UserService {
     constructor() {
@@ -11,6 +13,26 @@ class UserService {
             return user;
         } catch (error) {
             console.log("Something went wrong in the service layer");
+            throw error;
+        }
+    }
+
+    createToken(user) {
+        try {
+            const result = jwt.sign(user , JWT_KEY , {expiresIn: '1d'});
+            return result;
+        } catch (error) {
+            console.log("Something went wrong in the token creation");
+            throw error;
+        }
+    }
+
+    verifyToken(token) {
+        try {
+            const response = jwt.verify(token , JWT_KEY);
+            return response;
+        } catch (error) {
+            console.log("Something went wrong in the token validation" , error);
             throw error;
         }
     }
