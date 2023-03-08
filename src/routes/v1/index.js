1eec4be9c934de3242b9ae8f32fa269180647dd4
Anvesh Karra <anveshkarra1234@gmail.com>
2023-03-08 16:06:55 +0530

    Added user isAuth functionality

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -14,4 +14,13 @@ router.post(
     AuthRequestValidators.validateUserAuth ,
     UserController.signIn); 
 
+router.get(
+    '/isAuthenticated' , 
+    UserController.isAuthenticated
+)
+
+router.get('/dummy' , (req , res) => {
+    return res.status(200).json({message: 'OK'});
+})
+
 module.exports = router;
